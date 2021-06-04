8cfc59f03b21d17f4c45722098a8688632a66ed5
lmmfxl <lmmfxl@163.com>
2021-06-04 15:54:17 +0800

    products is doing

--- js/main.js
+++ js/main.js
@@ -5,14 +5,11 @@ $(function () {
 
     function changeImage() {
         var windowWidth = $(window).width();
-        console.log(windowWidth);
         var isSmallScreen = windowWidth < 768;
         $("#main_ad .carousel-inner > .item").each(function (i,element) {
                 var $item = $(element);
                var data = isSmallScreen ? $item.data('xs'):$item.data('lg');
-               // $item.css("backgroundImage",'url("'+ data +'")');
             if (isSmallScreen){
-                console.log(data);
                 $item.html('<img src="' + data +'"/>');
             }else {
                 $item.empty();
