759f6529cf6de1451e8ba8367808c0016e0c2420
lmmfxl <lmmfxl@163.com>
2021-06-10 18:02:07 +0800

    partner is doing

--- js/main.js
+++ js/main.js
@@ -21,7 +21,33 @@ $(function () {
 
     $(window).on("resize",changeImage).trigger('resize');
 
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /*
+    * 控制标签页容器宽度
+    * */
+
+    var $ulContainer = $('.nav-tabs');
+    //获取所有子元素的和
+    var width = 30;
+    // 遍历子元素
+    $ulContainer.children().each(function (i,ele) {
+       width += ele.clientWidth;
+    });
+    if (width > ($(window).width()-30)){
+        $ulContainer
+            .css('width',width)
+            .parent().css('overflow-x','scroll');
+    }
+
+    // a点击注册事件
+    var $newTitle = $('.new-title');
+    $('#news .nav-pills a').on('click',function () {
+        var $this = $(this);
+        var title = $this.data('title');
+        $newTitle.text(title);
 
 
+    })
 
 });
