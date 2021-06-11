a0fe2eb623aab06f72a18306128a0be4163f6a9d
lmmfxl <lmmfxl@163.com>
2021-06-11 11:36:09 +0800

    wjs is finished

--- js/main.js
+++ js/main.js
@@ -42,10 +42,28 @@ $(function () {
 
     // a点击注册事件
     var $newTitle = $('.new-title');
+    var starX;
+    var endX;
     $('#news .nav-pills a').on('click',function () {
         var $this = $(this);
         var title = $this.data('title');
         $newTitle.text(title);
+    })
+
+
+    //轮播图滑动事件
+    var $carousel = $('#carousel-example-generic');
+    $carousel.on('touchstart',function (e) {
+        starX = e.originalEvent.touches[0].clientX;
+    })
+    $carousel.on('touchmove',function (e) {
+        endX = e.originalEvent.touches[0].clientX;
+    })
+    $carousel.on('touchend',function (e) {
+        var distance = Math.abs(starX-endX);
+        if (distance > 50){
+            $(this).carousel(starX > endX ? 'next' : 'prev');
+        }
 
 
     })
